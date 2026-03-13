1fc09715702067f6e6084005666643232f309348
Guilherme Garcia <gui.garcia67@gmail.com>
2026-03-13 17:31:36 -0300

    process-feeds: save logs to separate file per run

--- process-feeds.sh
+++ process-feeds.sh
@@ -6,5 +6,6 @@ set -e # exit on error
 set -u # error on undefined variables
 
 LOGS_DIR="/data/process-feeds"
+timestamp=$(date -u +'%Y%m%d%H%M%S')
 
-docker exec brcrawl_app bash -c "uv run flask process-feeds > $LOGS_DIR"
+docker exec brcrawl_app bash -c "uv run flask process-feeds > $LOGS_DIR/$timestamp.log"
