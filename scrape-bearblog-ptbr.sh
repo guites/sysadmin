2160d22be8449d92c0c4aa1345119a13a90b5400
guites <gui.garcia67@gmail.com>
2026-07-14 18:18:02 -0300

    finish bearblog scrape script

--- scrape-bearblog-ptbr.sh
+++ scrape-bearblog-ptbr.sh
@@ -5,4 +5,39 @@
 set -e # exit on error
 set -u # error on undefined variables
 
-docker exec brcrawl_app bash -c "cd scraper/ && USER_AGENT='brcrawl (+https://brcrawl.guilhermegarcia.dev)' uv run scrapy bearblog_discover -a latest=bearblog-urls.txt -O bearblog-rss.jsonlprocess-feeds 2>&1 | tee $LOGS_DIR/$timestamp.log"
+USER_AGENT="brcrawl (+https://brcrawl.guilhermegarcia.dev)"
+BASE_DIR="/data/scrape-bearblog-ptbr"
+SCRAPE_OUTPUT="$BASE_DIR/rss.jsonl"
+SCRAPE_CMD="uv run scrapy bearblog_discover -O $SCRAPE_OUTPUT"
+
+LATEST_FILE="$BASE_DIR/latest.txt"
+LATEST=$(cat "$LATEST_FILE")
+
+URLS_FILE="$BASE_DIR/urls.txt"
+RSS_FILE="$BASE_DIR/rss.jsonl"
+
+if [ ! -z "$LATEST" ]; then
+    SCRAPE_CMD="$SCRAPE_CMD -a latest='$LATEST'"
+fi
+
+docker exec brcrawl_app bash <<EOF
+cd scraper/
+USER_AGENT="$USER_AGENT" "$SCRAPE_CMD"
+HAS_OUTPUT=$(head -n1 "$SCRAPE_OUTPUT")
+if [ -z "$HAS_OUTPUT" ]; then
+    echo "No new posts since last run. Exiting." && exit 0;
+fi
+echo "Updating latest scrapped post"
+echo "$HAS_OUTPUT" | jq -r .post_url > "$LATEST_FILE"
+
+echo "Generating list of scrapped blogs"
+cat "$SCRAPE_OUTPUT" | jq -r .blog_url | sort -u > "$URLS_FILE"
+
+echo "Getting the rss feed of each blog"
+USER_AGENT="$USER_AGENT" uv run scrapy crawl rss -a urls_file="$URLS_FILE" -o "$RSS_FILE"
+
+echo "Importing rss feeds"
+uv run flask import-feeds "$RSS_FILE"
+
+echo "Done! Exiting."
+EOF
