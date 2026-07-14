785c6e5634ef7b352b4e76fa7c42836096380f6e
Guilherme Garcia <gui.garcia67@gmail.com>
2026-07-14 19:15:16 -0300

    working scrape bearblog script

--- scrape-bearblog-ptbr.sh
+++ scrape-bearblog-ptbr.sh
@@ -6,31 +6,25 @@ set -e # exit on error
 set -u # error on undefined variables
 
 USER_AGENT="brcrawl (+https://brcrawl.guilhermegarcia.dev)"
-BASE_DIR="/data/scrape-bearblog-ptbr"
-SCRAPE_OUTPUT="$BASE_DIR/rss.jsonl"
-SCRAPE_CMD="uv run scrapy bearblog_discover -O $SCRAPE_OUTPUT"
+UV="/home/ubuntu/.local/bin/uv"
+SCRAPER_DIR="/home/ubuntu/brcrawl/scraper"
+LOCAL_DIR="/home/ubuntu/data/brcrawl/scrape-bearblog-ptbr"
 
-LATEST_FILE="$BASE_DIR/latest.txt"
+LATEST_FILE="$LOCAL_DIR/latest.txt"
+SCRAPE_OUTPUT="$LOCAL_DIR/rss.jsonl"
+URLS_FILE="$LOCAL_DIR/urls.txt"
+RSS_FILE="$LOCAL_DIR/rss.jsonl"
 
-URLS_FILE="$BASE_DIR/urls.txt"
-RSS_FILE="$BASE_DIR/rss.jsonl"
-
-docker exec -i \
-    -e USER_AGENT="$USER_AGENT" \
-    -e SCRAPE_OUTPUT="$SCRAPE_OUTPUT" \
-    -e LATEST_FILE="$LATEST_FILE" \
-    -e URLS_FILE="$URLS_FILE" \
-    -e RSS_FILE="$RSS_FILE" \
-    -e SCRAPE_CMD="$SCRAPE_CMD" \
-    brcrawl_app bash <<'EOF'
+SCRAPE_CMD="$UV run scrapy crawl bearblog_discover -O $SCRAPE_OUTPUT"
 
 LATEST=$(cat "$LATEST_FILE")
 if [ ! -z "$LATEST" ]; then
-    SCRAPE_CMD="$SCRAPE_CMD -a latest='$LATEST'"
+    SCRAPE_CMD="$SCRAPE_CMD -a latest=$LATEST"
 fi
 
-cd scraper/
-USER_AGENT="$USER_AGENT" "$SCRAPE_CMD"
+cd "$SCRAPER_DIR"
+USER_AGENT="$USER_AGENT" $SCRAPE_CMD
+
 HAS_OUTPUT=$(head -n1 "$SCRAPE_OUTPUT")
 if [ -z "$HAS_OUTPUT" ]; then
     echo "No new posts since last run. Exiting." && exit 0;
@@ -42,10 +36,13 @@ echo "Generating list of scrapped blogs"
 cat "$SCRAPE_OUTPUT" | jq -r .blog_url | sort -u > "$URLS_FILE"
 
 echo "Getting the rss feed of each blog"
-USER_AGENT="$USER_AGENT" uv run scrapy crawl rss -a urls_file="$URLS_FILE" -o "$RSS_FILE"
+USER_AGENT="$USER_AGENT" "$UV" run scrapy crawl rss -a urls_file="$URLS_FILE" -o "$RSS_FILE"
 
 echo "Importing rss feeds"
-uv run flask import-feeds "$RSS_FILE"
+docker exec -i brcrawl_app bash -c "uv run flask import-feeds /data/scrape-bearblog-ptbr/rss.jsonl --feed-status verified"
+
+echo "Removing artifacts"
+rm "$URLS_FILE"
+rm "$RSS_FILE"
 
 echo "Done! Exiting."
-EOF
